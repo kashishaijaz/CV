3076851c11413782bbc1b20b165feab96d233ccf
kashishaijaz <90380552+kashishaijaz@users.noreply.github.com>
2023-01-09 20:23:42 +0530

    Add initial CV Website file

---
+++ script.js
@@ -0,0 +1,26 @@
+alert("hello from head");
+
+// document.write("hi kashish");
+
+var username="kashish";
+var number=123;
+alert(username);
+
+var arrayName= new Array(10,20,30,40,50,60,70,80,90);
+
+alert(arrayName[6]);
+
+var Name="Luna";
+var age=3;
+var maritialStatus="Still Single";
+
+
+var number1=3456;
+var number2= 0702;
+function addnumbers(){
+var calculation= number1 + number2 ;
+
+ document.getElementById('divContent').innerHTML = calculation;
+}
+
+
